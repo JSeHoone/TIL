4cae2773dd797ba9cd947f168a346eb128289c25
JSeHoone <beebea545@gmail.com>
2023-11-25 10:39:20 +0900

    Start : Django web Project - make projectfolder is name shrinkers & app folder is name shortner

---
+++ django_practice/Zen_of_python.py
@@ -0,0 +1,6 @@
+# python 정신 (20개)
+
+# 1. Readablity counts (가독성이 좋아야 한다 !) -> 강사님이 가장 중요하게 생각함
+
+# 자세하게 보려면 python console에서 import this 하면 됨
+import this
---
+++ django_practice/readme.md
@@ -0,0 +1,2 @@
+1. shrinkers folder는 프로젝트 폴더
+2. shortner folder는 앱 폴더 
