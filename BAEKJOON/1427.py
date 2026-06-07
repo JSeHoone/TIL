6dd33f2aa567697d74ae806e0250631bb4d72900
JSeHoone <beebea545@gmail.com>
2026-06-07 17:56:34 +0900

    delete : Backjoon and Programmers Folder

--- BAEKJOON/1427.py
+++
@@ -1,7 +0,0 @@
-## 1427
-import sys
-data = sys.stdin.readline().rstrip()
-a = ""
-for i in sorted(data, reverse = True):
-    a += i
-print(a)
--- Programmers/12939.py
+++
@@ -1,6 +0,0 @@
-def solution(s):
-    
-    list_ = list(map(int, s.split()))
-    answer = str(min(list_)) + ' ' + str(max(list_))
-    
-    return answer
